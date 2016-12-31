356817ec1d4f65edbfe4829045991f22fa102398
robfinch <robfinch@sympatico.ca>
2016-12-31 08:51:32 -0500

    DSD - config defines for life game

--- DSD/DSD9/trunk/rtl/verilog/life/life.v
+++ DSD/DSD9/trunk/rtl/verilog/life/life.v
@@ -22,9 +22,12 @@
 // You should have received a copy of the GNU General Public License        
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.    
 //                                                                          
-// 4010 LUTs / 30 BRAM's (Artix7)
+// 4010 LUTs / 30 BRAM's (Artix7) (512x256 areana)
 // ============================================================================
 //
+`define HCELLS  256
+`define VCELLS  256
+`define VAMSB   7
 
 // Memory for life cells.
 // The environment is 512x256
@@ -32,22 +35,22 @@
 module liferam(wclk, wce, wadr, i, rclk, rce, ra0, ra1, ra2, rclk3, ra3, o0, o1, o2, o3);
 input wclk;
 input wce;
-input [7:0] wadr;
-input [511:0] i;
+input [`VAMSB:0] wadr;
+input [`HCELLS-1:0] i;
 input rclk;
 input rce;
-input [7:0] ra0;
-input [7:0] ra1;
-input [7:0] ra2;
+input [`VAMSB:0] ra0;
+input [`VAMSB:0] ra1;
+input [`VAMSB:0] ra2;
 input rclk3;
-input [7:0] ra3;
-output [511:0] o0;
-output [511:0] o1;
-output [511:0] o2;
-output [511:0] o3;
+input [`VAMSB:0] ra3;
+output [`HCELLS-1:0] o0;
+output [`HCELLS-1:0] o1;
+output [`HCELLS-1:0] o2;
+output [`HCELLS-1:0] o3;
 
-reg [511:0] mem [0:255];
-reg [7:0] rra0, rra1, rra2, rra3;
+reg [`HCELLS-1:0] mem [0:`VCELLS-1];
+reg [`VAMSB:0] rra0, rra1, rra2, rra3;
 
 always @(posedge wclk)
     if (wce) mem[wadr] <= i;
@@ -82,31 +85,31 @@ endmodule
 // Calculate life for an entire row of cells.
 
 module lifecalc_parallel(row0, row1, row2, alive);
-input [511:0] row0;
-input [511:0] row1;
-input [511:0] row2;
-output [511:0] alive;
+input [`HCELLS-1:0] row0;
+input [`HCELLS-1:0] row1;
+input [`HCELLS-1:0] row2;
+output [`HCELLS-1:0] alive;
 
 genvar g;
 generate
 begin : lifecal
-for (g = 0; g < 512; g = g + 1)
+for (g = 0; g < `HCELLS; g = g + 1)
 begin : lf1
 lifecalc ul (
     .self(row1[g]),
     .n({
     // three cells above
-    (g==511 ? {row0[0],row0[511],row0[510]} :
-       g==0 ? {row0[1],row0[0],row0[511]} :  
-               row0[(g+1):(g-1)]),
+    (g==`HCELLS-1 ? {row0[0],row0[`HCELLS-1],row0[`HCELLS-2]} :
+             g==0 ? {row0[1],row0[0],row0[`HCELLS-1]} :  
+                     row0[(g+1):(g-1)]),
     // cell on either side
-    (g==511 ? {row1[0],row1[510]} :
-       g==0 ? {row1[1],row1[511]} :  
-              {row1[(g+1)],row1[(g-1)]}),
+    (g==`HCELLS-1 ? {row1[0],row1[`HCELLS-2]} :
+             g==0 ? {row1[1],row1[`HCELLS-1]} :  
+                    {row1[(g+1)],row1[(g-1)]}),
     // three cells below
-    (g==511 ? {row2[0],row2[511],row2[510]} :
-       g==0 ? {row2[1],row2[0],row2[511]} :  
-               row2[(g+1):(g-1)])}),
+    (g==`HCELLS-1 ? {row2[0],row2[`HCELLS-1],row2[`HCELLS-2]} :
+             g==0 ? {row2[1],row2[0],row2[`HCELLS-1]} :  
+                     row2[(g+1):(g-1)])}),
     .alive(alive[g])
 );
 end
@@ -130,20 +133,30 @@ input hsync;
 input [23:0] rgb_i;
 output reg [23:0] rgb_o;
 
-reg [31:0] cnt;
+// Control regs
 reg [31:0] freq;
-wire [7:0] radr = cnt[25:18];
-reg [7:0] wadr0,wadr1,wadr2,wadr3;
-wire [511:0] alive;
-reg [511:0] alive1,alive2,alive3;
-reg [9:0] scanline;
+reg [23:0] color;
+reg [23:0] bkcolor;
+reg [11:0] window_left;                 // position of left edge of window
+reg [11:0] window_top;                  // position of top edge of window
+reg scalex,scaley;                      // screen scaling 0=1:1, 1 = 2:1
+reg tbk;                                // transparent background
+reg disp;                               // display enable
+reg calc;                               // calculation enable
+reg load;
+
+reg [31:0] cnt;
+wire [`VAMSB:0] radr = cnt[`VAMSB+18:18];
+reg [`VAMSB:0] wadr0,wadr1,wadr2,wadr3;
+wire [`HCELLS-1:0] alive;
+reg [`HCELLS-1:0] alive1,alive2,alive3;
+reg [11:0] scanline;
 reg [11:0] dotcnt;
 wire pe_hsync,pe_vsync;
-wire [511:0] row0, row1, row2, row3;
+wire [`HCELLS-1:0] row0, row1, row2, row3;
 reg [511:0] d;
-reg [511:0] q,q1;
-reg load,calc,disp;
-reg [7:0] load_row;
+reg [`HCELLS-1:0] q,q1;
+reg [`VAMSB:0] load_row;
 
 wire cs = cyc_i && stb_i && (adr_i[31:16]==16'hFFD3);
 assign ack_o = cs;
@@ -156,12 +169,19 @@ if (rst_i) begin
     load <= 1'b0;
     disp <= 1'b1;
     freq <= 32'h10;
+    color <= 24'hFF_FF_FF;
+    bkcolor <= 24'h00_00_00;
+    tbk <= 1'b0;
+    window_left <= 12'd240;
+    window_top <= 12'hF80;
+    scalex <= 1'b1;
+    scaley <= 1'b1;
 end
 else begin
     load <= 1'b0;
     if (cs) begin
         if (we_i)
-            casex(adr_i[6:2])
+            case(adr_i[6:2])
             5'b00000:   d[31:0] <= dat_i;
             5'b00001:   d[63:32] <= dat_i;
             5'b00010:   d[95:64] <= dat_i;
@@ -178,13 +198,26 @@ else begin
             5'b01101:   d[447:416] <= dat_i;
             5'b01110:   d[479:448] <= dat_i;
             5'b01111:   d[511:480] <= dat_i;
-            5'b10000:   load_row <= dat_i[7:0];
+            5'b10000:   load_row <= dat_i[`VAMSB:0];
             5'b10001:   begin
                         load <= dat_i[0];
                         calc <= dat_i[1];
                         disp <= dat_i[2];
                         end
             5'b10010:   freq <= dat_i;
+                        // color control
+            5'b10011:   color <= dat_i;
+            5'b10100:   begin
+                        bkcolor <= dat_i[23:0];
+                        tbk <= dat_i[31];
+                        end
+                        // window control
+            5'b10101:   begin
+                        window_left <= dat_i[11:0];
+                        window_top <= dat_i[27:16];
+                        scalex <= dat_i[15];
+                        scaley <= dat_i[31];
+                        end
             default:    ;
             endcase
     end
@@ -195,14 +228,14 @@ liferam u1
     .wclk(clk_i),
     .wce(load|(calc&cnt[17])),
     .wadr(load ? load_row : wadr3),
-    .i(load ? d : alive3),
+    .i(load ? d[`HCELLS-1:0] : alive3),
     .rclk(clk_i),
     .rce(cnt[17]),
     .ra0(radr-8'd1),
     .ra1(radr),
     .ra2(radr+8'd1),
     .rclk3(pe_hsync),
-    .ra3(scanline[8:1]),
+    .ra3(scaley ? scanline[8:1] : scanline[7:0]),
     .o0(row0),
     .o1(row1),
     .o2(row2),
@@ -260,13 +293,14 @@ edge_det edv1
 	.ee()
 );
 
-wire blank = scanline[9] || (dotcnt < 240) || (dotcnt > 1265);
+wire blank = (scaley ? |scanline[11:9] : |scanline[11:8]) ||
+            (dotcnt < window_left) || (dotcnt > window_left + (`HCELLS << scalex));
 
 always @(posedge vclk)
     if (pe_vsync)
-        scanline <= 10'h380;
+        scanline <= window_top;
     else if (pe_hsync)
-        scanline <= scanline + 10'd1;
+        scanline <= scanline + 12'd1;
 always @(posedge vclk)
     if (pe_hsync)
         dotcnt <= 12'd0;
@@ -276,13 +310,13 @@ always @(posedge vclk)
 always @(posedge vclk)
     if (pe_hsync)
         q <= row3;
-    else if (!blank & dotcnt[0])
-        q <= {q[510:0],1'b0};
+    else if (!blank & (scalex ? dotcnt[0] : 1'b1))
+        q <= {q[`HCELLS-2:0],1'b0};
 
 always @(posedge vclk)
-    if (blank)
+    if (blank|~disp)
         rgb_o <= rgb_i;
     else
-        rgb_o <= q[511] ? 24'hFF_FF_FF : 24'h00_00_00; 
+        rgb_o <= q[`HCELLS-1] ? color : tbk ? rgb_i : bkcolor; 
 
 endmodule
